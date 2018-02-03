82bf220236d94f2008985e7fbe2aae403e9604e9
songhm <songhm@qssec.com>
2018-02-03 18:05:09 +0800

    引用vue-resource组件

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 import goods from './components/goods/goods.vue';
 import seller from './components/seller/seller.vue';
 import ratings from './components/ratings/ratings.vue';
@@ -9,6 +10,7 @@ import ratings from './components/ratings/ratings.vue';
 import './common/stylus/index.styl';
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
 
 let app = Vue.extend(App);
 let router = new VueRouter({
