9de2b3715c3bc3d7b3b031dfc35600492341c48a
songhm <songhm@qssec.com>
2018-02-06 15:39:58 +0800

    增加首页模糊效果

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -27,6 +27,9 @@
       <span class="bulletin-text">{{ seller.bulletin }}</span>
       <i class="icon-keyboard_arrow_right"></i>
     </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
+    </div>
   </div>
 </template>
 
@@ -38,7 +41,7 @@
       }
     },
     created () {
-      this.classMap = ['decrease', 'discount', 'sepcial', 'guarantee', 'invoice'];
+      this.classMap = ['decrease', 'discount', 'special', 'guarantee', 'invoice'];
     }
   };
 
@@ -49,8 +52,9 @@
   @import "../../common/stylus/mixin";
 
   .header
+    position relative
     color: #fff
-    background: #099
+    background rgba(7,17,27,0.5)
     .content-wrapper
       position relative
       padding: 24px 12px 18px 24px
@@ -155,4 +159,13 @@
           right 12px
           top 8px
 
+    .background
+      position absolute
+      top 0
+      left 0
+      width 100%
+      height 100%
+      z-index -1
+      /*产生模糊效果*/
+      filter blur(10px)
 </style>
