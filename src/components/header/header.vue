784660fd566726ba9e3b71f6dd6cd7241ef152a6
songhm <songhm@qssec.com>
2018-02-06 16:30:37 +0800

    增加弹出层

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,12 +17,12 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      <div v-if="seller.supports" class="support-count">
+      <div v-if="seller.supports" class="support-count" @click="showDetail">
         <span class="count">{{seller.supports.length}}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bulletin-wrapper">
+    <div class="bulletin-wrapper" @click="showDetail">
       <span class="bulletin-title"></span>
       <span class="bulletin-text">{{ seller.bulletin }}</span>
       <i class="icon-keyboard_arrow_right"></i>
@@ -30,6 +30,7 @@
     <div class="background">
       <img :src="seller.avatar" width="100%" height="100%">
     </div>
+    <div v-show="detailShow" class="detail"></div>
   </div>
 </template>
 
@@ -38,6 +39,16 @@
     props: {
       seller: {
         type: Object
+      }
+    },
+    data () {
+      return {
+        detailShow: false
+      };
+    },
+    methods: {
+      showDetail () {
+        this.detailShow = true;
       }
     },
     created () {
@@ -53,6 +64,7 @@
 
   .header
     position relative
+    overflow hidden
     color: #fff
     background rgba(7,17,27,0.5)
     .content-wrapper
@@ -143,7 +155,7 @@
       .bulletin-title
         display inline-block
         vertical-align top
-        margin-top 7px
+        margin-top 8px
         width 22px
         height 12px
         bg-image('bulletin')
@@ -168,4 +180,14 @@
       z-index -1
       /*产生模糊效果*/
       filter blur(10px)
+    .detail
+      position fixed
+      z-index 100
+      top: 0
+      left 0
+      width 100%
+      height 100%
+      overflow auto
+      background rgba(7,17,27,0.8)
+
 </style>
